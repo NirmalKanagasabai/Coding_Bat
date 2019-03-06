2c28d88690e6a34d02d6db25b8be6652bd746308
Nirmal Kanagasabai <nirmal.kanagasabai@gmail.com>
2019-03-05 23:44:56 -0500

    String_E4

---
+++ Java_Problems/src/Strings/Easy/E4_makeOutWord.java
@@ -0,0 +1,25 @@
+package Strings.Easy;
+
+/*
+
+Given an "out" string length 4, such as "<<>>", and a word, 
+return a new string where the word is in the middle of the out string, e.g. "<<word>>". 
+Note: use str.substring(i, j) to extract the String starting at index i and going up to but not including index j.
+
+
+makeOutWord("<<>>", "Yay") → "<<Yay>>"
+makeOutWord("<<>>", "WooHoo") → "<<WooHoo>>"
+makeOutWord("[[]]", "word") → "[[word]]"
+
+*/
+
+public class E4_makeOutWord {
+	
+	public static void main (String[] args) {
+		makeOutWord("<<>>", "Yay");
+	}
+	
+	public static String makeOutWord(String out, String word) {
+		return out.substring(0,2) + word + out.substring(2);
+	}
+}
