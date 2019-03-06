ea5b67003b4d4b747ad88fad48eaadb8660a4169
Nirmal Kanagasabai <nirmal.kanagasabai@gmail.com>
2019-03-06 00:11:20 -0500

    String_E6

---
+++ Java_Problems/src/Strings/Easy/E6_firstTwo.java
@@ -0,0 +1,32 @@
+package Strings.Easy;
+
+/*
+
+Given a string, return the string made of its first two chars, so the String "Hello" yields "He". 
+If the string is shorter than length 2, return whatever there is, so "X" yields "X", and the empty string "" yields the empty string "". 
+Note that str.length() returns the length of a string.
+
+
+firstTwo("Hello") → "He"
+firstTwo("abcdefg") → "ab"
+firstTwo("ab") → "ab"
+
+*/
+
+public class E6_firstTwo {
+	
+	public static void main (String[] args) {
+		firstTwo("Hello");
+	}
+	
+	public static String firstTwo(String str) {
+		String result = "";
+		int length = str.length();
+		if (length >= 2) {
+			result = str.substring(0, 2);
+		} else {
+			result = str;
+		}
+		return result;
+	}
+}
