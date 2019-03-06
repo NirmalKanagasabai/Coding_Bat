ba15e00cb001a75918c223feed26c94c804951df
Nirmal Kanagasabai <nirmal.kanagasabai@gmail.com>
2019-03-05 23:40:59 -0500

    String_E3

--- Java_Problems/src/Strings/Easy/E2_makeAbba.java
+++ Java_Problems/src/Strings/Easy/E2_makeAbba.java
@@ -2,7 +2,8 @@ package Strings.Easy;
 
 /*
 
-Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+Given two strings, a and b, return the result of putting them together in the order abba, 
+e.g. "Hi" and "Bye" returns "HiByeByeHi".
 
 
 makeAbba("Hi", "Bye") → "HiByeByeHi"
