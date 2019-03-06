802f2cc026c66d92fde7ea0d92d51a9347100112
Nirmal Kanagasabai <nirmal.kanagasabai@gmail.com>
2019-03-06 00:15:51 -0500

    String_E8

---
+++ Java_Problems/src/Strings/Easy/E8_withoutEnd.java
@@ -0,0 +1,23 @@
+package Strings.Easy;
+
+/*
+
+Given a string, return a version without the first and last char, so "Hello" yields "ell". 
+The string length will be at least 2.
+
+withoutEnd("Hello") → "ell"
+withoutEnd("java") → "av"
+withoutEnd("coding") → "odin"
+
+*/
+
+public class E8_withoutEnd {
+	
+	public static void main (String[] args) {
+		withoutEnd("Hello");
+	}
+	
+	public static String withoutEnd(String str) {
+		return str.substring(1, str.length()-1);
+	}
+}
