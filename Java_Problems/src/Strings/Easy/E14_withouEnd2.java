c7dbddf8d9f9d37b165ddf091323d799830107b5
Nirmal Kanagasabai <nirmal.kanagasabai@gmail.com>
2019-03-06 00:35:20 -0500

    String_E14

---
+++ Java_Problems/src/Strings/Easy/E14_withouEnd2.java
@@ -0,0 +1,30 @@
+package Strings.Easy;
+
+/*
+
+Given a string, return a version without both the first and last char of the string. 
+The string may be any length, including 0.
+
+
+withouEnd2("Hello") → "ell"
+withouEnd2("abc") → "b"
+withouEnd2("ab") → ""
+
+*/
+
+public class E14_withouEnd2 {
+	
+	public static void main (String[] args) {
+		withouEnd2("Hello");
+	}
+	
+	public static String withouEnd2(String str) {
+		int length = str.length();
+		
+		if (length >= 2) {
+			return str.substring(1, length-1);
+		} else {
+			return "";
+		}
+	}
+}
