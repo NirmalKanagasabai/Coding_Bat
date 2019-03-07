771a5c8de63b72f3315af8ee61b1d0f0e8a2475f
Nirmal Kanagasabai <nirmal.kanagasabai@gmail.com>
2019-03-06 21:05:36 -0500

    String_E29

---
+++ Java_Problems/src/Strings/Easy/E29_without2.java
@@ -0,0 +1,29 @@
+package Strings.Easy;
+
+/*
+
+Given a string, if a length 2 substring appears at both its beginning and end, 
+return a string without the substring at the beginning, so "HelloHe" yields "lloHe". 
+The substring may overlap with itself, so "Hi" yields "". Otherwise, return the original string unchanged.
+
+
+without2("HelloHe") → "lloHe"
+without2("HelloHi") → "HelloHi"
+without2("Hi") → ""
+
+*/
+
+public class E29_without2 {
+	
+	public static void main (String[] args) {
+		System.out.println(without2("HelloHe"));
+	}
+	
+	public static String without2(String str) {
+		if (str.length() >= 2 && str.substring(0, 2).equals(str.substring(str.length()-2))) {
+			return str.substring(2);
+		} else {
+			return str;
+		}
+	}
+}
