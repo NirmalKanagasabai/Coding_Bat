ec77d89b58d56118a25fb34b5c95175e640d82e4
Nirmal Kanagasabai <nirmal.kanagasabai@gmail.com>
2019-03-08 19:25:00 -0500

    String_M4

---
+++ Java_Problems/src/Strings/Medium/M4_countCode.java
@@ -0,0 +1,31 @@
+package Strings.Medium;
+
+/*
+
+Return the number of times that the string "code" appears anywhere in the given string, 
+except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+
+countCode("aaacodebbb") → 1
+countCode("codexxcode") → 2
+countCode("cozexxcope") → 2
+
+*/
+
+public class M4_countCode {
+	
+	public static void main (String[] args) {
+		System.out.println(countCode("catdog"));
+	}
+	
+	public static int countCode(String str) {
+		int count = 0;
+
+		for (int i=0; i<str.length()-3; i++) {
+			if(str.substring(i, i+2).equals("co") && str.charAt(i+3) == 'e') {
+				count++;
+			}
+		}
+		return count;
+	}
+
+}
