19b70773fbe67a43a6f3ae14938834c2367f8aa4
Nirmal Kanagasabai <nirmal.kanagasabai@gmail.com>
2019-03-09 13:18:07 -0500

    String_M17

---
+++ Java_Problems/src/Strings/Medium/M17_oneTwo.java
@@ -0,0 +1,39 @@
+package Strings.Medium;
+
+/*
+
+Given a string, compute a new string by moving the first char to come after the next two chars, 
+so "abc" yields "bca". Repeat this process for each subsequent group of 3 chars, so "abcdef" yields "bcaefd". 
+Ignore any group of fewer than 3 chars at the end.
+
+oneTwo("abc") → "bca"
+oneTwo("tca") → "cat"
+oneTwo("tcagdo") → "catdog"
+
+*/
+
+public class M17_oneTwo {
+	
+	public static void main (String[] args) {
+		System.out.println(oneTwo("bcaefdxy"));
+	}
+	
+	public static String oneTwo(String str) {
+		int length = str.length();
+		StringBuffer sb = new StringBuffer();
+		String output = "";
+		if (length < 3) {
+			output = str;
+		} else {
+			System.out.println("STR: " + str);
+			for (int i=0; i<length-2; i=i+3) {
+				String threeCharString = str.substring(i, i+3);
+				System.out.println("Three Char String: " + threeCharString);
+				sb.append(threeCharString.substring(1, 3));
+				sb.append(threeCharString.charAt(0));
+			}
+			//sb.append(str.substring(length - length % 3));
+		}
+		return sb.toString();
+	}
+}
