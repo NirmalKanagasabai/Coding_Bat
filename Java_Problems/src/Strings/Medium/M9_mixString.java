ef65b2e728e4d2b3a07b723fec17ffeed33cf968
Nirmal Kanagasabai <nirmal.kanagasabai@gmail.com>
2019-03-08 21:15:02 -0500

    String_M9

---
+++ Java_Problems/src/Strings/Medium/M9_mixString.java
@@ -0,0 +1,48 @@
+package Strings.Medium;
+
+import java.util.Stack;
+
+/*
+
+Given two strings, a and b, create a bigger string made of the first char of a, 
+the first char of b, the second char of a, the second char of b, and so on. 
+Any leftover chars go at the end of the result.
+
+mixString("abc", "xyz") → "axbycz"
+mixString("Hi", "There") → "HTihere"
+mixString("xxxx", "There") → "xTxhxexre"
+
+*/
+
+public class M9_mixString {
+	
+	public static void main (String[] args) {
+		System.out.println(mixString("xxxx", "There"));
+	}
+	
+	public static String mixString(String a, String b) {
+	  StringBuffer output = new StringBuffer();
+	  int aLen = a.length();
+	  int bLen = b.length();
+	  
+	  if (aLen > bLen) {
+		  output.append(prepareLargerString(a, b, bLen));
+		  output.append(a.substring(bLen));
+	  } else {
+		  output.append(prepareLargerString(a, b, aLen));
+		  output.append(b.substring(aLen));
+	  }
+	  return output.toString();
+	}
+	
+	public static String prepareLargerString(String a, String b, int len) {
+		StringBuffer sb = new StringBuffer();
+		for (int i=0; i<len; i++) {
+			sb.append(a.charAt(i));
+			sb.append(b.charAt(i));
+		}
+		
+		return sb.toString();
+	}
+
+}
